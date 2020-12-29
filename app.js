aad9aff18f130f5c1d572e523f50cc5e2378ba17
Sohan Agashimani <sohanagashimani@users.noreply.github.com>
2020-12-29 13:27:34 +0530

    i/p o/p processing done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 console.log(txtInput)
 function clickHandler() {
+    outputDiv.innerText = "sasaasa" + txtInput.value;
 
     console.log("clicked");
     console.log("input", txtInput.value);
